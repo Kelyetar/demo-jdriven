d0fa9e5a11f77ecd58780d87124c224be0e6d4a1
Matthijs Nieuwdorp <matthijs.nieuwdorp@gmail.com>
2025-03-08 08:57:49 +0000

    Initial

---
+++ src/main/java/personal/nieuwdorp/demojdriven/DemoJdrivenApplication.java
@@ -0,0 +1,13 @@
+package personal.nieuwdorp.demojdriven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJdrivenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJdrivenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/personal/nieuwdorp/demojdriven/DemoJdrivenApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.nieuwdorp.demojdriven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJdrivenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
