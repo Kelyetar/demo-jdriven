7d4bde86189ae8c086ee19774ad07eb2ccee74b8
Matthijs Nieuwdorp <matthijs.nieuwdorp@gmail.com>
2025-03-11 20:21:35 +0000

    Front-end

--- src/main/java/personal/nieuwdorp/demojdriven/controller/ProductController.java
+++ src/main/java/personal/nieuwdorp/demojdriven/controller/ProductController.java
@@ -1,5 +1,6 @@
 package personal.nieuwdorp.demojdriven.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import personal.nieuwdorp.demojdriven.controller.domain.ProductResponse;
@@ -9,6 +10,7 @@ import personal.nieuwdorp.demojdriven.repository.domain.Product;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+@CrossOrigin()
 @RestController
 public class ProductController {
     private final ProductRepository productRepository;
